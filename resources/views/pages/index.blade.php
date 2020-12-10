a4d443c3bb8e5100004558b9c889a6ea07f4c649
Duong Trong Phuc <dtrongphucdv@gmail.com>
2020-12-10 21:32:17 +0700

    fix font

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,59 +6,45 @@
     <main class="main mt-4">
         <div class="container">
             <div class="row">
-                <div class="col-12">
-                    <div class="main-filter d-flex">
-                        <div class="main-filter__left d-flex align-items-center">
-                            <p class="main-filter__left--title">Sắp xếp theo</p>
-                            <ul class="main-filter__left--list">
-                                <li class="filter-list__item filter-list__item--active">Phổ biến</li>
-                                <li class="filter-list__item">Mới nhất</li>
-                                <li class="filter-list__item">Bán chạy</li>
-                                <li class="filter-list__item filter-list__item--select">
-                                    Giá
-                                    <i class="fas fa-chevron-down"></i>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="main-filter__right"></div>
-                    </div>
+                <div class="col-2">
+                    <ul class="categories">
+                        <li class="category-item category-item--heading">
+                            <i class="fas fa-list"></i>
+                            <p>Tất cả danh mục</p>
+                        </li>
+                    </ul>
                 </div>
-                <div class="col-12">
-                    <div class="main-products">
-                        <div class="row">
-                            <div class="col-3">
-                                <div class="product-item">
-                                    Product
-                                </div>
+                <div class="col-10">
+                    <div class="col-12">
+                        <div class="main-filter d-flex">
+                            <div class="main-filter__left d-flex align-items-center">
+                                <p class="main-filter__left--title">Sắp xếp theo</p>
+                                <ul class="main-filter__left--list">
+                                    <li class="filter-list__item filter-list__item--active">Phổ biến</li>
+                                    <li class="filter-list__item">Mới nhất</li>
+                                    <li class="filter-list__item">Bán chạy</li>
+                                    <li class="filter-list__item filter-list__item--select">
+                                        Giá
+                                        <i class="fas fa-chevron-down"></i>
+                                    </li>
+                                </ul>
                             </div>
-                            <div class="col-3">
-                                <div class="product-item">
-                                    Product
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="product-item">
-                                    Product
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="product-item">
-                                    Product
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="product-item">
-                                    Product
-                                </div>
-                            </div>
-                            <div class="col-3">
-                                <div class="product-item">
-                                    Product
+                            <div class="main-filter__right"></div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="main-products">
+                            <div class="row">
+                                <div class="col-3">
+                                    <div class="product-item">
+                                        Product
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
         </div>
     </main>
