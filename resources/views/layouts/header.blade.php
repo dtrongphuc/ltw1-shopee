44006c1836edc460d314f5aba002c931e2474cf3
ThanhPhu-Dev <tphu2000@gmail.com>
2020-12-11 22:48:25 +0700

    xong phan header o giua

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,35 +1,26 @@
-{{-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+    <link rel="stylesheet" href="./header.css">
     <title>hearder chạy thử</title>
 </head>
 
-<body> --}}
+<body class="body">
     <header>
         <div>
             <div class="header">
                 <div class="grid">
                     <nav class="header__navbar">
                         <ul class="header_narbar-list ">
-                            <li class="header__navbar-item header__navbar-item-separate">Kênh Người Bán</li>
-                            <li class="header__navbar-item header__navbar-item-separate">Tải ứng Dụng</li>
-                            <li class="header__navbar-item ">
-                                Kết Nối
-                                <a href="" class="header__navbar-icon-link">
-                                    <i class="header__navbar-icon fab fa-facebook"></i>
-                                </a>
-                                <a href="" class="header__navbar-icon-link">
-                                    <i class="header__navbar-icon fab fa-instagram-square"></i>
-                                </a>
-                            </li>
+                            <!-- <li class="header__navbar-item header__navbar-item-separate">Kênh Người Bán</li>
+                            <li class="header__navbar-item header__navbar-item-separate">Tải ứng Dụng</li> -->
                         </ul>
                         <ul class="header_narbar-list ">
-                            <li class="header__navbar-item">
+                            <!-- <li class="header__navbar-item">
                                 <a href="" class="header__navbar-item-link">
                                     <i class="header__navbar-icon far fa-bell"></i>
                                     Thông Báo
@@ -39,6 +30,15 @@
                                 <a href="" class="header__navbar-item-link">
                                     <i class="header__navbar-icon far fa-question-circle"></i>
                                     Trợ Giúp
+                                </a>
+                            </li> -->
+                            <li class="eader__navbar-item header__navbar-item--strong" style="display:inline-block;">
+                                Kết Nối
+                                <a href="" class="header__navbar-icon-link">
+                                    <i class="header__navbar-icon fab fa-facebook" style="color: white;"></i>
+                                </a>
+                                <a href="" class="header__navbar-icon-link">
+                                    <i class="header__navbar-icon fab fa-instagram-square" style="color: white;"></i>
                                 </a>
                             </li>
                             <li class="header__navbar-item header__navbar-item--strong header__navbar-item-separate">Đăng Ký</li>
@@ -51,12 +51,14 @@
                             <input class="input-search" type="text" placeholder="Tìm sản Phẩm, Thương Hiệu, và tên shoppe">
                             <span class="button-search"><i class="fas fa-search search-icon"></i></span>
                         </div>
-                        <div class="cart-button"><i class="fas fa-shopping-cart"></i></div>
+                        <div class="cart-button">
+                            <span><i class="fas fa-shopping-cart" style="color: white; margin-top: 10px;"></i></span>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </header>
-{{-- </body>
+</body>
 
-</html> --}}
+</html>
