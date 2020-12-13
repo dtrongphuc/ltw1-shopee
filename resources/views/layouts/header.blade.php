a767b14010b940d5123dfc326c88e85461a68a68
Duong Trong Phuc <dtrongphucdv@gmail.com>
2020-12-13 21:09:05 +0700

    extend master layout blade

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-@extends('../layouts/master')
-
-<body class="body">
     <header>
         <div class="header">
             <div class="container">
@@ -11,15 +6,15 @@
                         <ul class="header_narbar-list ">
                             <!-- <li class="header__navbar-item header__navbar-item-separate">Kênh Người Bán</li>
                                 <li class="header__navbar-item header__navbar-item-separate">Tải ứng Dụng</li> -->
-                                <li class="header__navbar-item header__navbar-item--strong" style="display:inline-block;">
-                                    Kết nối
-                                    <a href="" class="header__navbar-icon-link">
-                                        <i class="header__navbar-icon fab fa-facebook" style="color: white;"></i>
-                                    </a>
-                                    <a href="" class="header__navbar-icon-link">
-                                        <i class="header__navbar-icon fab fa-instagram-square" style="color: white;"></i>
-                                    </a>
-                                </li>
+                            <li class="header__navbar-item header__navbar-item--strong" style="display:inline-block;">
+                                Kết nối
+                                <a href="" class="header__navbar-icon-link">
+                                    <i class="header__navbar-icon fab fa-facebook" style="color: white;"></i>
+                                </a>
+                                <a href="" class="header__navbar-icon-link">
+                                    <i class="header__navbar-icon fab fa-instagram-square" style="color: white;"></i>
+                                </a>
+                            </li>
                         </ul>
                         <ul class="header_narbar-list ">
                             <!-- <li class="header__navbar-item">
@@ -34,7 +29,7 @@
                                         Trợ Giúp
                                     </a>
                                 </li> -->
-                            
+
                             <li class="header__navbar-item header__navbar-item--strong header__navbar-item-separate"><a
                                     href="#">Đăng ký</a></li>
                             <li class="header__navbar-item header__navbar-item--strong"><a href="#">Đăng nhập</a></li>
@@ -50,26 +45,33 @@
                                 <div class="search">
                                     <input class="input-search" type="text"
                                         placeholder="Tìm sản Phẩm, Thương Hiệu, và tên shoppe">
-                                    <span class="button-search d-flex align-items-center justify-content-center"><i class="fas fa-search search-icon"></i></span>
-                                    
+                                    <span class="button-search d-flex align-items-center justify-content-center"><i
+                                            class="fas fa-search search-icon"></i></span>
+
                                 </div>
                                 <div class="bottom-header">
                                     <ul class="bottom-items">
-                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ chữ A</a>
+                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ
+                                                chữ A</a>
                                         </li>
-                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ chữ A</a>
+                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ
+                                                chữ A</a>
                                         </li>
-                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ chữ A</a>
+                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ
+                                                chữ A</a>
                                         </li>
-                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ chữ A</a>
+                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ
+                                                chữ A</a>
                                         </li>
-                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ chữ A</a>
+                                        <li class="category-item--header"><a href="#" class="category-item--a">vòng cổ
+                                                chữ A</a>
                                         </li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="cart-button">
-                                <span><i class="fas fa-shopping-cart" style="color: white; margin-top: 10px;"></i></span>
+                                <span><i class="fas fa-shopping-cart"
+                                        style="color: white; margin-top: 10px;"></i></span>
                             </div>
                         </div>
                     </div>
@@ -77,6 +79,3 @@
             </div>
         </div>
     </header>
-</body>
-
-</html>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,3 +1,6 @@
+</head>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,11 +9,19 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" >
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,400;1,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+   
     <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-    <title>Document</title>
+    <title>{{ ucfirst($title ?? '') }}</title>
 </head>
+<body>
+    @include('../layouts/header')
+    @section('body')
+
+    @show
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
