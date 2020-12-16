3cbcaada3b3d983ccb07ddecefd3e709c4188818
Duong Trong Phuc <dtrongphucdv@gmail.com>
2020-12-16 16:26:01 +0700

    Product images slider

---
+++ resources/js/product.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    let maxItems = $('.product-images__slider').children('div').length;
+    $('.product-images__slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: maxItems >= 5 ? 5 : maxItems,
+        adaptiveHeight: true,
+        slidesToScroll: 1,
+        prevArrow: $('.product-images__ctn--left'),
+        nextArrow: $('.product-images__ctn--right'),
+        // responsive: [
+        //   {
+        //     breakpoint: 1024,
+        //     settings: {
+        //       slidesToShow: 3,
+        //       slidesToScroll: 3,
+        //       infinite: true,
+        //       dots: true
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 600,
+        //     settings: {
+        //       slidesToShow: 2,
+        //       slidesToScroll: 2
+        //     }
+        //   },
+        //   {
+        //     breakpoint: 480,
+        //     settings: {
+        //       slidesToShow: 1,
+        //       slidesToScroll: 1
+        //     }
+        //   }
+        //   // You can unslick at a given breakpoint now by adding:
+        //   // settings: "unslick"
+        //   // instead of a settings object
+        // ]
+    });
+});
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,10 +18,10 @@
     @section('body')
 
     @show
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-
+    <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
 </body>
 </html>
