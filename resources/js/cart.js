2bba0d716791e9db79927dba8f071d9ee96e742a
nguyenhieunghia2001 <67048599+nguyenhieunghia2001@users.noreply.github.com>
2021-01-02 12:53:52 +0700

    js cập nhật địa chỉ

--- resources/js/cart.js
+++ resources/js/cart.js
@@ -20,60 +20,6 @@ const {
     compile
 } = require("vue-template-compiler");
 
-//button tăng
-// function UpQuantity(product) {
-//     var productName = product.getAttribute("data-cart");
-//     var quantity = document.getElementById("quantity_" + productName).value;
-//     document.getElementById("quantity_" + productName).value = parseInt(quantity) + 1;
-//     var price = document.getElementById("price_" + productName).innerHTML;
-//     // toltalprice_
-//     var totalprice = parseInt(price.substring(0, price.length - 4)) * (parseInt(quantity) + 1);
-//     //giá tổng
-//     var payall = document.getElementById("payall").innerHTML;
-//     var temp = payall.substring(0, payall.length - 4);
-//     var price_all = parseInt(temp.replace(/,/i, ''));
-//     var payallprice = price_all + parseInt(price.substring(0, price.length - 4));
-//     if (totalprice > 999) {
-//         totalprice = totalprice.toString().substring(0, totalprice.toString().length - 3) + "," + totalprice
-//             .toString().substring(1);
-//     }
-//     if (payallprice > 999) {
-//         payallprice = payallprice.toString().substring(0, payallprice.toString().length - 3) + "," + payallprice
-//             .toString().substring(1);
-//     }
-//     document.getElementById("payall").innerHTML = payallprice + ",000";
-//     document.getElementById("toltalprice_" + productName).innerHTML = totalprice + ",000";
-// }
-
-// //giảm số lượng
-// function DownQuantity(product) {
-//     var productName = product.getAttribute("data-cart");
-//     var quantity = document.getElementById("quantity_" + productName).value;
-//     if (parseInt(quantity) > 1) {
-//         document.getElementById("quantity_" + productName).value = parseInt(quantity) - 1;
-//         var price = document.getElementById("price_" + productName).innerHTML;
-//         // toltalprice_
-//         var totalprice = parseInt(price.substring(0, price.length - 4)) * (parseInt(quantity) - 1);
-
-//         var payall = document.getElementById("payall").innerHTML;
-//         var temp = payall.substring(0, payall.length - 4);
-//         var price_all = parseInt(temp.replace(/,/i, ''));
-//         var payallprice = price_all - parseInt(price.substring(0, price.length - 4));
-//         if (totalprice > 999) {
-//             totalprice = totalprice.toString().substring(0, totalprice.toString().length - 3) + "," + totalprice
-//                 .toString().substring(1);
-//         }
-//         if (payallprice > 999) {
-//             payallprice = payallprice.toString().substring(0, payallprice.toString().length - 3) + "," +
-//                 payallprice.toString().substring(1);
-//         }
-//         document.getElementById("payall").innerHTML = payallprice + ",000";
-//         document.getElementById("toltalprice_" + productName).innerHTML = totalprice + ",000";
-//     }
-
-
-// }
-
 //tăng số lượng
 $(document).ready(function() {
     $("button").on("click", function(event) {
@@ -104,7 +50,6 @@ $(document).ready(function() {
 
             }
         });
-
     });
 });
 
@@ -128,6 +73,3 @@ function StringToMoney(str) {
         str = str.substring(0, str.length - 3) + "," + str.substring(1);
     return str;
 }
-// function printdata(data) {
-//     alert(data);
-// }
