8e8440a16c9f54f7f546685796e2c3b9091dbae7
Duong Trong Phuc <dtrongphucdv@gmail.com>
2021-01-03 15:31:11 +0700

    Chuyển trang với tất cả query string

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
                     (self::productsWithCategoryAndFilter($request->category, $request->filter)) :
                     ($isHasCategory ? self::productsWithCategory($request->category) :
                     ($isHasFilter ? self::productsWithFilter($request->filter) :
-                    Product::orderBy('likeCount', 'desc')->paginate(10)));
+                    Product::orderBy('likeCount', 'desc')->paginate(10)->withQueryString()));
         return view('pages.index',[
             'category' => $category,
             'products' => $products
@@ -38,13 +38,13 @@ class HomeController extends Controller
 
         $querySearch = $request ->input('querySearch');
 
-        $productSearch = Product::where('productName', 'like', "%$querySearch%")->paginate(10);
+        $productSearch = Product::where('productName', 'like', "%$querySearch%")->paginate(10)->withQueryString();
 
         return view('pages.searchResults',['productSearch'=> $productSearch]);
     }
 
     public function productsWithCategory($categoryId) {
-        return Product::where('categoryId', '=', $categoryId)->paginate(10);
+        return Product::where('categoryId', '=', $categoryId)->paginate(10)->withQueryString();
     }
 
     public function productsWithFilter($filter) {
@@ -62,7 +62,7 @@ class HomeController extends Controller
             default:
                 $orderBy = 'likeCount';
         }
-        return Product::orderBy($orderBy, 'desc')->paginate(10);
+        return Product::orderBy($orderBy, 'desc')->paginate(10)->withQueryString();
     }
 
     public function productsWithCategoryAndFilter($category, $filter) {
@@ -82,6 +82,6 @@ class HomeController extends Controller
         }
 
         return Product::where('categoryId', '=', $category)
-                        ->orderBy($orderBy, 'desc')->paginate(10);
+                        ->orderBy($orderBy, 'desc')->paginate(10)->withQueryString();
     }
 }
